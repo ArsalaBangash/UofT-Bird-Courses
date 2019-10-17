3fdd717412809e2a791e36a24a11c78b82537e84
Niveditha Kani <nivedithakani@gmail.com>
2019-10-17 16:34:57 -0400

    Get course names and rating from website, store in dictionary

--- course-evals-scraper/scraper.py
+++ course-evals-scraper/scraper.py
@@ -5,6 +5,7 @@ from getpass import getpass
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.remote.webelement import WebElement
+import time
 
 quercus_url = "https://q.utoronto.ca/courses/48756/external_tools/291"
 evals_url = "https://course-evals.utoronto.ca/BPI/fbview.aspx?blockid=hjeZ7JJWJupVgjPoyu&userid=tO4GQugFiFULB0AXgInh7idHCU-AnN3pNhvC&lng=en"
@@ -21,10 +22,10 @@ password_input: WebElement = driver.find_element_by_id("password")
 login_button: WebElement = driver.find_element_by_name("_eventId_proceed")
 
 # Prompts user for UTORID and password and logs into acorn with those details
-utorid = input("UTORID: ")
-utorid_input.send_keys()
-password = getpass()
-password_input.send_keys(password)
+# utorid = input("UTORID: ")
+utorid_input.send_keys("utorid")
+# password = getpass()
+password_input.send_keys("password")
 login_button.click()
 
 # Once you're loggin in, navigate to the course evals URL
@@ -33,14 +34,31 @@ driver.get(evals_url)
 
 # Elements from the course evals data page
 hundred_button: WebElement = driver.find_element_by_css_selector("#fbvGridPageSizeSelectBlock > select:nth-child(1) > option:nth-child(7)")
-next_button: WebElement = driver.find_element_by_css_selector(".gPaging > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(6) > input:nth-child(1)")
-print(next_button)
+grid: WebElement = driver.find_element_by_class_name("wsgridview")
+rows = grid.find_elements_by_tag_name("tr")
+
 # Fetch 100 evals at a time. 
+time.sleep(2)
 hundred_button.click()
+rows = []
+bird_courses = {} 
+
+time.sleep(2)
+for i in range(64):    ## Hardcoded. 
+    next_button: WebElement = driver.find_element_by_css_selector(".gPaging > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(6) > input:nth-child(1)")
+    table_rows = grid.find_elements_by_tag_name("tr")
+    for table_row in range(1, len(table_rows)):
+        rows.append(table_rows[table_row].text)
+        item = table_rows[table_row].find_elements_by_tag_name("td")
+        bird_courses[item[1].text] = item[6].text
+    next_button.click()
+    time.sleep(2)
+print (bird_courses)
 
 # This is the html we get from the page
 html: property = driver.page_source
 soup = BeautifulSoup(html, 'html')
+
 
 # For now, all we do with the beautiful soup data model of our page is write it to an index.html
 # file. #TODO: Parse this data and get the course eval info we're looking for
